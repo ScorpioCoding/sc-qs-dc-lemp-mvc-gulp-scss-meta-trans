7dd52c1ac45e4fb3a4ca717076b2d0f464625656
denOldTimer <kribo@outlook.com>
2023-01-28 22:22:20 +0100

    restructuring

--- html/App/Routes/site.routes.php
+++ html/App/Routes/site.routes.php
@@ -5,4 +5,11 @@ return (object) array(
   ''    => ['lang' => 'en', 'module' => 'site', 'namespace' => 'App\Controllers\Site', 'controller' => 'Home', 'action' => 'index'],
   '/'   => ['lang' => 'en', 'module' => 'site', 'namespace' => 'App\Controllers\Site', 'controller' => 'Home', 'action' => 'index'],
 
+
+  '/{lang}/home'   => ['module' => 'site', 'namespace' => 'App\Controllers\Site', 'controller' => 'Home', 'action' => 'index'],
+
+
+
+
+
 );
